1de5589713c7a28fd593cb82d3ef19392f5bed84
Luca Regini <luca.regini@avanade.com>
2019-06-23 15:27:46 +0200

    Powershell script to create a snapshot for all the managed disks of all vms in a given Azure resource group

---
+++ New-SnapshotForRG.ps1
@@ -0,0 +1,27 @@
+﻿function New-SnapshotForRG
+{
+
+    Param ([string]$subName,[string]$rgName)
+
+    Connect-AzAccount
+    
+    $subscription=Get-AzSubscription -SubscriptionName $subName
+
+    Set-AzContext -Subscription $subscription.SubscriptionId
+
+    $disks=Get-AzResource -ResourceType Microsoft.Compute/Disks -ResourceGroupName $rgName 
+    $location = 'WestEurope'
+
+    Foreach ($disk in $disks)
+    {
+       #Write-Host $disk.Id
+       $snapshotName=-join($disk.Name,"_snapshot")
+       #Write-Host $newname
+
+       $snapshotConfig = New-AzSnapshotConfig -SourceUri $disk.Id -Location $location -CreateOption copy
+       New-AzSnapshot -Snapshot $snapshotConfig -SnapshotName $snapshotName -ResourceGroupName $rgName
+
+    }
+
+}
+
